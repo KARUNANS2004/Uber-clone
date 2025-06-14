0e17f7c69f614a87d035e221e124afee3e156868
KARUNANS2004 <s.karuna0074@gmail.com>
2025-06-14 15:27:45 +0530

    error check

--- Backend/controllers/ride.controller.js
+++ Backend/controllers/ride.controller.js
@@ -6,6 +6,7 @@ const rideModel = require('../models/ride.model')
 
 module.exports.createRide=async (req,res)=>{
     const errors=validationResult(req);
+    console.log(req)
     if(!errors.isEmpty()){
         return res.status(400).json({errors:errors.array()})
     }
