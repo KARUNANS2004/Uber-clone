41615790fe8a90162f55a894fd67c5cbd986916d
KARUNANS2004 <s.karuna0074@gmail.com>
2025-07-30 18:37:44 +0530

    change in start screen

--- frontend/src/components/Start.tsx
+++ frontend/src/components/Start.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Start = () => {
   return (
     <div >
-      <div className='h-screen bg-center bg-cover bg-[url(\start_background.png)] pt-8 w-full flex justify-between flex-col'>
+      <div className='h-screen bg-center bg-cover bg-[url(/start_background.png)] pt-8 w-full flex justify-between flex-col'>
         <img className='w-16 ml-8 scale-[1.5]' src="\saarthi_pages_logo.png" alt="" />
         <div className='bg-[#D4932D] pb-7 py-4 px-4'>
           <h2 className='text-3xl font-bold  text-[#FAF6EF]'>Begin Your Journey with Sarthi</h2>
